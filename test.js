b8c05cc1c6089a0c53826885f7b2c06e841f2ca2
Danilo Torrisi <danilo.torrisi@me.com>
2021-02-02 12:42:34 +0100

    Added more functions.

--- test.js
+++ test.js
@@ -3,3 +3,11 @@ console.log('Hello World my friend')
 function addFunction() {
     return 1 + 1;
 }
+
+function removeFunction() {
+    return 1 - 1;
+}
+
+function divideFunction() {
+    return 1  /1;
+}
